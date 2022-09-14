aa77bed00272b7c9527247fd02e16320aeb9265d
Mitchell Hudson <soggybag@gmail.com>
2022-09-13 21:37:00 -0700

    Update example-9.ts

--- example-9.ts
+++ example-9.ts
@@ -2,7 +2,7 @@
 
 // Currently the function below uses a string for
 // for the type of monster. Better to use an enum
-// since monsters are only: lizard, flying, ape
+// since monsters are only: lizard, flying, or ape
 
 // Define an enum for monster type
 
